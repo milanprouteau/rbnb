cd7aec904148e8ddfde98f754827e4b67e4cbfaf
AbdelrhmanAbdalla <abdelrhman92.abdalla@gmail.com>
2020-07-09 10:31:00 +0200

    Add RBNB FORM

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {decode, encode} from 'base-64'
 import Icon from 'react-native-vector-icons/FontAwesome';
 import { createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from "react-navigation-tabs";
@@ -16,15 +17,30 @@ import Profil from "./screens/Profil";
 import Search from "./screens/Search";
 import Login from "./screens/Login";
 import * as firebase from 'firebase';
+import firestore from 'firebase/firestore'
+import rbnb from './screens/rbnb';
+
+if (!global.btoa) { global.btoa = encode }
+if (!global.atob) { global.atob = decode }
 
 const firebaseConfig = {
     apiKey: "AIzaSyAqg9mrXrJI1H-hplYJptff5K7ogvpYj7A",
     authDomain: "rbnb-a870e.firebaseapp.com",
     databaseURL: "https://rbnb-a870e.firebaseio.com",
+    projectId: "rbnb-a870e",
     storageBucket: "rbnb-a870e.appspot.com",
+    messagingSenderId: "230943438573",
+    appId: "1:230943438573:web:ebf72fe2c8c4418dc8bee4"
 }
 
-firebase.initializeApp(firebaseConfig);
+try {
+    firebase.initializeApp(firebaseConfig);
+    } catch (err) {
+    if (!/already exists/.test(err.message)) {
+    console.error('Firebase initialization error', err.stack)
+    }
+    }
+firebase.firestore();
 
 
 const persistConfig = {key: 'root', storage: AsyncStorage};
@@ -59,6 +75,18 @@ const BottomNavigator = createBottomTabNavigator(
                 )
             })
         },
+        RBNB: {
+            screen: rbnb,
+            navigationOptions: () => ({
+                tabBarIcon: ({tintColor}) => (
+                    <Icon
+                        name={"rbnb"}
+                        color={tintColor}
+                        size={24}
+                    />
+                )
+            })
+        },
         Profil: {
             screen: Profil,
             navigationOptions: () => ({
@@ -87,6 +115,7 @@ const AppNavigator = createStackNavigator(
         Splash: {screen: Splash, navigationOptions: {headerShown: false}},
         Login: {screen: Login, navigationOptions: {headerShown: false}},
         Home: {screen: BottomNavigator, navigationOptions: {headerShown: false}},
+        Rbnb: {screen: rbnb, navigationOptions: {headerShown: false}},
         Details: {screen: Details, navigationOptions: {headerShown: false}},
     },
     {
--- screens/rbnb.js
+++ screens/rbnb.js
@@ -1,37 +1,112 @@
+// screens/rbnb.js
+
 import React, { Component } from 'react';
+import { Button, StyleSheet, TextInput, ScrollView, ActivityIndicator, View } from 'react-native';
+import * as firebase from 'firebase';
 
-export default class Rbnb extends Component {
-
-  static navigationOptions = ({ navigation }) => {
-    return {
-    }
-  };
-
-  state = {
-    rbnb: {
+class rbnb extends Component {
+  constructor() {
+    super();
+    this.state = {
       name: '',
       description: '',
+    };
+  }
+
+  inputValueUpdate = (val, prop) => {
+    const state = this.state;
+    state[prop] = val;
+    this.setState(state);
+  }
+
+  storeRbnb() {
+    if(this.state.name === ''){
+     alert('Remplissez le nom de votre RBNB!')
+    }else if(this.state.description === ''){
+        alert('Remplissez la description de votre RBNB!')
+       } else {
+      this.setState({
+        isLoading: true,
+      });      
+      firebase.firestore().collection('rbnbs').add({
+        name: this.state.name,
+        description: this.state.description,
+      }).then((res) => {
+        this.setState({
+          name: '',
+          description: '',
+          isLoading: false,
+        });
+        this.props.navigation.navigate('Home')
+      })
+      .catch((err) => {
+        console.error("Error found: ", err);
+        this.setState({
+          isLoading: false,
+        });
+      });
     }
   }
 
   render() {
+    if(this.state.isLoading){
+      return(
+        <View style={styles.preloader}>
+          <ActivityIndicator size="large" color="#9E9E9E"/>
+        </View>
+      )
+    }
     return (
-        <View style={styles.container}>
-        <TextInput
-          value={props.values.name}
-          placeholder='Name'
-          onChangeText={text => { props.setFieldValue('name', text) }}
-        />
-        <TextInput
-          value={props.values.category}
-          placeholder='Description'
-          onChangeText={text => { props.setFieldValue('description', text) }}
-        />
-        <Button
-          title='Submit'
-          onPress={() => props.handleSubmit()}
-        />
-      </View>
+      <ScrollView style={styles.container}>
+        <View style={styles.inputGroup}>
+          <TextInput
+              placeholder={'Name'}
+              value={this.state.name}
+              onChangeText={(val) => this.inputValueUpdate(val, 'name')}
+          />
+        </View>
+        <View style={styles.inputGroup}>
+          <TextInput
+              multiline={true}
+              numberOfLines={4}
+              placeholder={'Description'}
+              value={this.state.description}
+              onChangeText={(val) => this.inputValueUpdate(val, 'description')}
+          />
+        </View>
+        <View style={styles.button}>
+          <Button
+            title='Add RBNB'
+            onPress={() => this.storeRbnb()} 
+            color="#19AC52"
+          />
+        </View>
+      </ScrollView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 35
+  },
+  inputGroup: {
+    flex: 1,
+    padding: 0,
+    marginBottom: 15,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
+  },
+  preloader: {
+    left: 0,
+    right: 0,
+    top: 0,
+    bottom: 0,
+    position: 'absolute',
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+})
+
+export default rbnb;
